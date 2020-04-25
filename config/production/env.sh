6a9386ae004246edda0bc7624debc588725763e0
Tanmay Moharana <tanmay4osme@gmail.com>
2020-04-25 13:00:28 +0200

    update env.sh

--- config/production/env.sh
+++ config/production/env.sh
@@ -3,7 +3,7 @@
 # to the server side of your app by using process.env.MY_VAR
 #
 # Example:
-# export MONGO_URL="mongodb+srv://<username>:<password>@cluster0-6gbcy.mongodb.net/test?retryWrites=true&w=majority"
-# export ROOT_URL="https://vrclassroom.herokuapp.com"
+export MONGO_URL="mongodb+srv://<username>:<password>@cluster0-6gbcy.mongodb.net/test?retryWrites=true&w=majority"
+export ROOT_URL="https://vrclassroom.herokuapp.com"
 
 export SAMPLE_VARIABLE="somevalue"
